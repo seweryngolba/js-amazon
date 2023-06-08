5d26742f190f5dcafd973264f09c5f0e103233f1
Seweryn Golba <sgoolb@gmail.com>
2023-06-08 21:01:24 +0200

    add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,8 +46,8 @@ products.forEach((product) => {
     Added
   </div>
 
-  <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name= "${
-    product.name
+  <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id= "${
+    product.id
   }">Add to Cart</button>
 </div>
     `;
@@ -57,6 +57,25 @@ document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
-    console.log(button.dataset);
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+
+    console.log(cart);
   });
 });
