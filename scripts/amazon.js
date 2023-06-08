60b2478c0175cbb848f5332f600308e8d43b1ab3
Seweryn Golba <sgoolb@gmail.com>
2023-06-08 20:45:36 +0200

    cart script

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,9 @@ products.forEach((product) => {
     Added
   </div>
 
-  <button class="add-to-cart-button button-primary js-add-to-cart">Add to Cart</button>
+  <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name= "${
+    product.name
+  }">Add to Cart</button>
 </div>
     `;
 });
@@ -55,6 +57,6 @@ document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
-    console.log("hello");
+    console.log(button.dataset);
   });
 });
